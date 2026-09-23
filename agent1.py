1e51a55d00952ee9532c83639f72b1c2174a1e55
wal21017-boop <wal21017@byui.edu>
2026-09-23 14:59:35 -0600

    first local agent

--- agent1.py
+++ agent1.py
@@ -2,18 +2,49 @@
 
 # uv add langchain langchain-google-genai python-dotenv
 
+from dotenv import load_dotenv
+load_dotenv()
+
+from langchain.agents import create_agent
+from langgraph.checkpoint.memory import InMemorySaver
+from rich.console import Console
+from rich.markdown import Markdown
+
+from tools import starter_tool
+
+import uuid
+
+agent = create_agent(
+                model = "google_genai:gemini-3.6-flash",
+                checkpointer= InMemorySaver(), 
+                system_prompt = "return the number as a series of tallies, add previous numbers to the total"
+              )
+
+config = {"configurable": {"thread_id": str(uuid.uuid4())}}
+
+console = Console()
+
+
+
+# In main(), replace print(response) with:
+# console.print(Markdown(response))
+
 def get_response(prompt: str) -> str:
-    return prompt
+    response = agent.invoke({"messages": [{"role": "user", "content":prompt}]}
+                 , config = config)
+    return response["messages"][-1].text
 
 def main():
     while True:
         try:
             prompt = input("Input: ")
+            
             if prompt == "exit": break
             response = get_response(prompt)
         except EOFError:
             break
-        print(response)
+        console.print(Markdown(response))
+#        print(response)
 
 if __name__ == "__main__":
     main()
---
+++ tools.py
@@ -0,0 +1,5 @@
+def starter_tool(): 
+    """
+    This tool serves as a placeholder
+    """
+    return "nothing"
